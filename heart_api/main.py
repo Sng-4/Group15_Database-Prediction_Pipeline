716ac1aed8c28c52087b52c8be0605c1a78dfcd3
Emmanuel Dania <e.dania@alustudent.com>
2025-11-02 19:10:43 +0200

    Patient Model

--- heart_api/main.py
+++ heart_api/main.py
@@ -7,9 +7,14 @@ app = FastAPI(title="Heart Disease SQL API")
 
 # --- Connect to the hosted SQL database ---
 sql_conn = mysql.connector.connect(
-    host="your_host",
-    user="your_user",
-    password="your_password",
+    host="localhost",
+    user="root",
+    password="Naturi^06",
     database="heart_disease_db"
 )
 sql_cursor = sql_conn.cursor(dictionary=True)
+class Patient(BaseModel):
+    first_name: str = Field(..., min_length=1, max_length=100)
+    last_name: str = Field(..., min_length=1, max_length=100)
+    dob: date
+    gender: str = Field(..., regex="^(Male|Female|Other)$") 
